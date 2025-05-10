380609ace13e08f9d7abb39091473a3e77dc33a7
Dennis Kobert <dennis@kobert.dev>
2025-05-10 12:51:02 +0200

    Add log parser

--- uwuembed/src/main.rs
+++ uwuembed/src/main.rs
@@ -9,29 +9,11 @@ struct Position {
     coordinates: Vec<f64>,
 }
 
-#[derive(Debug)]
-enum Query {
-    Nearest {
-        k: u32,
-        point: Vec<f64>,
-    },
-    Sphere {
-        radius: f64,
-        min_corners: Vec<f64>,
-        max_corners: Vec<f64>,
-        points: Vec<f64>,
-    },
-    Box {
-        min_corners: Vec<f64>,
-        max_corners: Vec<f64>,
-    },
-}
 
 #[derive(Debug)]
 struct Iteration {
     number: usize,
     positions: Vec<Position>,
-    queries: Vec<Query>,
 }
 
 fn parse_positions_file<P: AsRef<Path>>(path: P) -> io::Result<Vec<Iteration>> {
@@ -55,7 +37,6 @@ fn parse_positions_file<P: AsRef<Path>>(path: P) -> io::Result<Vec<Iteration>> {
             current_iteration = Some(Iteration {
                 number: num,
                 positions: Vec::new(),
-                queries: Vec::new(),
             });
         } else if line.contains("---") {
             // End of positions block
@@ -98,119 +79,19 @@ fn parse_positions_file<P: AsRef<Path>>(path: P) -> io::Result<Vec<Iteration>> {
     Ok(iterations)
 }
 
-fn parse_queries_file<P: AsRef<Path>>(path: P, iterations: &mut [Iteration]) -> io::Result<()> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let mut current_iteration_idx = 0;
-    let mut lines = reader.lines();
-
-    while let Some(Ok(line)) = lines.next() {
-        if line.starts_with("ITERATION") {
-            let num = line.split_whitespace()
-                .nth(1)
-                .and_then(|s| s.parse::<usize>().ok())
-                .unwrap_or(0);
-                
-            current_iteration_idx = iterations.iter()
-                .position(|iter| iter.number == num)
-                .unwrap_or(0);
-        } else if line.contains("---") {
-            // Separator, skip
-            continue;
-        } else if line.starts_with("NEAREST") {
-            let parts: Vec<&str> = line.split_whitespace().collect();
-            let k = parts[1].parse::<u32>().unwrap_or(0);
-            let dim = parts[2].parse::<usize>().unwrap_or(0);
-            
-            let mut point = Vec::with_capacity(dim);
-            for i in 0..dim {
-                if let Some(val) = parts.get(3 + i) {
-                    if let Ok(coord) = val.parse::<f64>() {
-                        point.push(coord);
-                    }
-                }
-            }
-            
-            if let Some(iter) = iterations.get_mut(current_iteration_idx) {
-                iter.queries.push(Query::Nearest { k, point });
-            }
-        } else if line.starts_with("SPHERE") {
-            let parts: Vec<&str> = line.split_whitespace().collect();
-            let radius = parts[1].parse::<f64>().unwrap_or(0.0);
-            let dim = parts[2].parse::<usize>().unwrap_or(0);
-            
-            let mut min_corners = Vec::with_capacity(dim);
-            let mut max_corners = Vec::with_capacity(dim);
-            let mut points = Vec::with_capacity(dim);
-            
-            for i in 0..dim {
-                let base_idx = 3 + i * 3;
-                
-                if let (Some(min), Some(max), Some(point)) = (
-                    parts.get(base_idx).and_then(|v| v.parse::<f64>().ok()),
-                    parts.get(base_idx + 1).and_then(|v| v.parse::<f64>().ok()),
-                    parts.get(base_idx + 2).and_then(|v| v.parse::<f64>().ok())
-                ) {
-                    min_corners.push(min);
-                    max_corners.push(max);
-                    points.push(point);
-                }
-            }
-            
-            if let Some(iter) = iterations.get_mut(current_iteration_idx) {
-                iter.queries.push(Query::Sphere {
-                    radius,
-                    min_corners,
-                    max_corners,
-                    points,
-                });
-            }
-        } else if line.starts_with("BOX") {
-            let parts: Vec<&str> = line.split_whitespace().collect();
-            let dim = parts[1].parse::<usize>().unwrap_or(0);
-            
-            let mut min_corners = Vec::with_capacity(dim);
-            let mut max_corners = Vec::with_capacity(dim);
-            
-            for i in 0..dim {
-                let base_idx = 2 + i * 2;
-                
-                if let (Some(min), Some(max)) = (
-                    parts.get(base_idx).and_then(|v| v.parse::<f64>().ok()),
-                    parts.get(base_idx + 1).and_then(|v| v.parse::<f64>().ok())
-                ) {
-                    min_corners.push(min);
-                    max_corners.push(max);
-                }
-            }
-            
-            if let Some(iter) = iterations.get_mut(current_iteration_idx) {
-                iter.queries.push(Query::Box {
-                    min_corners,
-                    max_corners,
-                });
-            }
-        }
-    }
-    
-    Ok(())
-}
 
 fn main() -> io::Result<()> {
     let positions_path = "positions.log";
-    let queries_path = "queries.log";
     
     let mut iterations = parse_positions_file(positions_path)?;
-    parse_queries_file(queries_path, &mut iterations)?;
     
     // Print summary
     println!("Parsed {} iterations", iterations.len());
     for iter in &iterations {
         println!(
-            "Iteration {}: {} positions, {} queries", 
+            "Iteration {}: {} positions", 
             iter.number, 
             iter.positions.len(), 
-            iter.queries.len()
         );
     }
     
